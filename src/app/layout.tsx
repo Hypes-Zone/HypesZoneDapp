a67091c5a6d04fef3c05f9979a73ba733fcce355
hypessoon <hypessoon@gmail.com>
2025-02-16 21:24:30 +0100

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,27 @@ const links: { label: string; path: string }[] = [
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>Hypeszone Dapp</title>
+        <meta name="description" content="Decentralized Messaging Connect. Collaborate. Engage — Wallet-to-Wallet, AI-Driven, and 100% Permissionless." />
+
+        <meta itemProp="name" content="Hypeszone Dapp" />
+        <meta itemProp="description" content="Decentralized Messaging Connect. Collaborate. Engage — Wallet-to-Wallet, AI-Driven, and 100% Permissionless." />
+        <meta itemProp="image" content="https://app.hypes.zone/assets/img/favicon.png" />
+
+        <meta property="og:url" content="https://app.hypes.zone/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Hypeszone Dapp" />
+        <meta property="og:description" content="Decentralized Messaging Connect. Collaborate. Engage — Wallet-to-Wallet, AI-Driven, and 100% Permissionless." />
+        <meta property="og:image" content="https://app.hypes.zone/assets/img/favicon.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Hypeszone Dapp" />
+        <meta name="twitter:description" content="Decentralized Messaging Connect. Collaborate. Engage — Wallet-to-Wallet, AI-Driven, and 100% Permissionless." />
+        <meta name="twitter:image" content="https://app.hypes.zone/assets/img/favicon.png" />
+
+        <link rel="icon" href="../assets/img/favicon.png" />
+      </head>
       <body>
         <ReactQueryProvider>
           <ClusterProvider>
