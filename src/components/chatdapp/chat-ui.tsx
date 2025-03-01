cf64b7b1f5f332258e2226cdff8053f874737289
hypessoon <hypessoon@gmail.com>
2025-03-01 21:57:14 +0100

    wip: send chat

--- src/components/chatdapp/chat-ui.tsx
+++ src/components/chatdapp/chat-ui.tsx
@@ -7,6 +7,8 @@ import { useWallet } from "@solana/wallet-adapter-react";
 export function ChatUiApp() {
   const {publicKey} = useWallet();
 
+  const [text, setText] = useState('');
+
   if (!publicKey) {
     return (
       <></>
@@ -14,17 +16,33 @@ export function ChatUiApp() {
   }
 
   const setActive = (e: any) => {
-    console.log('Setting active')
     let current = document.getElementsByClassName("chat-box-active");
     if (current.length > 0) {
       current[0].className = current[0].className.replace(" chat-box-active", "");
     }
+
+    let slugValue = e.currentTarget.textContent.replace(/\s+/g, '-').toLowerCase();
+
+    let chatBoxChats = document.getElementsByClassName("md:chat-box-chat");
+    for (let i = 0; i < chatBoxChats.length; i++) {
+      if (chatBoxChats[i].id !== "chat-" + slugValue) {
+        chatBoxChats[i].className = chatBoxChats[i].className.replace(" md:chat-box-chat-active", "");
+      } else {
+        chatBoxChats[i].className += " md:chat-box-chat-active";
+      }
+    }
+
     e.currentTarget.className += " chat-box-active";
+
   }
 
   const startNewChat = (e: any) => {
-    console.log('Starting new chat')
     const {value} = document.querySelector(e.target.getAttribute("data-input"));
+
+    if (!value) {
+      return;
+    }
+
     document.querySelector(e.target.getAttribute("data-input")).value = '';
 
     // create li element
@@ -41,19 +59,84 @@ export function ChatUiApp() {
 
     // append li to ul as first child in the list
     document.querySelector(e.target.getAttribute("data-items")).prepend(li);
+    //
+    // let chat = ` <div className="md:col-span-2 md:chat-box md:chat-box-chat">
+    //
+    //   <div className="chat chat-start fade-in-txt-1">
+    //     <div className="chat-bubble chat-bubble-primary">Remote</div>
+    //   </div>
+    //
+    //   <div className="chat chat-end fade-in-txt-1">
+    //     <div className="chat-bubble chat-bubble-accent">
+    //       Local
+    //     </div>
+    //   </div>
+    //
+    // </div>`;
+
+    // Create the chat-box-chat
+    let chatBoxChat = document.createElement("div");
+    chatBoxChat.className = "md:col-span-2 md:chat-box md:chat-box-chat";
+
+    let slugValue = value.replace(/\s+/g, '-').toLowerCase();
+    chatBoxChat.id = "chat-" + slugValue;
+
+    document.getElementById("chat-container")?.prepend(chatBoxChat);
   }
 
+  const sendChatEnter = (e: any) => {
+    if (e.keyCode === 13 && !e.shiftKey) {  // enter key
+      sendChat();
+    }
+  }
+
+  const sendChat = () => {
+    const value = text;
+    setText('');
+
+    if (!value || value === '' || value.length === 0 || value.trim() === '' || value.length > 1024) {
+      return;
+    }
+
+    let chatBoxChatActive = document.getElementsByClassName("md:chat-box-chat-active");
+    if (chatBoxChatActive.length === 0 || chatBoxChatActive.length > 1) {
+      return;
+    }
+
+
+    // create chat bubble
+    let chat = document.createElement("div");
+    chat.classList.add("chat");
+    chat.classList.add("chat-end");
+    chat.classList.add("fade-in-txt-1");
+
+    // create chat bubble
+    let chatBubble = document.createElement("div");
+    chatBubble.classList.add("chat-bubble");
+    chatBubble.classList.add("chat-bubble-accent");
+    chatBubble.innerText = value;
+
+
+    // append chat bubble to chat
+    chat.appendChild(chatBubble);
+
+    // append chat to chat-box-chat
+    chatBoxChatActive[0].appendChild(chat);
+
+    // scroll to bottom
+    chatBoxChatActive[0].scrollTop = chatBoxChatActive[0].scrollHeight;
+  }
 
   return (
 
     <div className="grid grid-flow-col grid-cols-3 md:grid-rows-1 gap-4 chat-box">
 
       <div className="md:col-span-1 chat-box-list md:chat-box">
-
         <div className="grid grid-rows-2">
 
           <div>
-            <button className="btn btn-secondary text-white-50 btn-wide btn-center" data-input="#id-input" data-items="#chat-box-list-items"
+            <button className="btn btn-secondary text-white-50 btn-wide btn-center" data-input="#id-input"
+                    data-items="#chat-box-list-items"
                     onClick={(e) => startNewChat(e)}>Start New Chat
             </button>
           </div>
@@ -65,51 +148,28 @@ export function ChatUiApp() {
         </div>
 
         <div id="chat-box-scroll">
-          <ul id="chat-box-list-items" className="menu menu-lg bg-base-200 rounded-box">
-
-          </ul>
-
+          <ul id="chat-box-list-items" className="menu menu-lg bg-base-200 rounded-box"></ul>
         </div>
-
 
       </div>
 
-      <div className="md:col-span-2 md:chat-box">
+      <div id="chat-container" className="md:col-span-2 md:chat-container">
 
-        <div className="chat chat-start fade-in-txt-2">
-          <div className="chat-bubble chat-bubble-primary">What kind of nonsense is this</div>
-        </div>
+        <div className="chat-box-bottom">
+          <div className="chat-box-input">
+            <textarea id="send-input" value={text} className="input input-bordered chat-input" placeholder=". . ."
+                      onChange={e => setText(e.target.value)} onKeyUp={sendChatEnter}></textarea>
+          </div>
 
-        <div className="chat chat-start fade-in-txt-4">
-          <div className="chat-bubble chat-bubble-secondary">
-            Put me on the Council and not make me a Master!??
+          <div className="chat-box-send">
+            <button className="btn btn-secondary btn-wide btn-center" onClick={sendChat}>Send
+            </button>
           </div>
         </div>
-
-        <div className="chat chat-start fade-in-txt-6">
-          <div className="chat-bubble chat-bubble-accent">
-            That&apos;s never been done in the history of the Jedi. It&apos;s insulting!
-          </div>
-        </div>
-
-        <div className="chat chat-end fade-in-txt-8">
-          <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
-        </div>
-
-        <div className="chat chat-end fade-in-txt-10">
-          <div className="chat-bubble chat-bubble-success">You have been given a great honor.</div>
-        </div>
-
-        <div className="chat chat-end fade-in-txt-12">
-          <div className="chat-bubble chat-bubble-warning">To be on the Council at your age.</div>
-        </div>
-
-        <div className="chat chat-end fade-in-txt-14">
-          <div className="chat-bubble chat-bubble-error">It&apos;s never happened before.</div>
-        </div>
-
 
       </div>
+
+
     </div>
   )
 
